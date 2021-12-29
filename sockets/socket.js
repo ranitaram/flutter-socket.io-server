68c5406d91aae968859fb9b0618dad25d4b8ba64
ramses indaldecio <ramsesindaldecio@Mac-mini-de-ramses.local>
2021-12-28 21:06:19 -0700

    cambios sobre bandas terminado

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,20 @@
 const {io} = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+bands.addBand(new Band('Panda'));
+bands.addBand(new Band('Heroes del silencio'));
+bands.addBand(new Band('Metallica'));
+bands.addBand(new Band('Hombre G'));
+
+console.log(bands);
 
 //Mensajes de Sockets
 
 io.on('connection', client => {
-    console.log('Cliente coectado');
+    console.log('Cliente conectado');
+    client.emit('active-bands', bands.getBands());
     client.on('disconnect', () => { 
         console.log('Cliente desconectado');
      });
@@ -12,4 +23,20 @@ io.on('connection', client => {
 
          io.emit('mensaje', {admin: 'Nuevo mensaje'});
      });
+     client.on('vote-band', (payload ) => {
+         bands.voteBand(payload.id);
+         io.emit('active-bands', bands.getBands());
+     });
+
+     client.on('add-band', (payload ) => {
+         const newBand = new Band(payload.name)
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('delete-band', (payload ) => {
+        
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
   });
